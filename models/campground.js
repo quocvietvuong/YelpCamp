15a23bf01e68d1f747973d1f2c034d56ae851d11
Quoc Viet Vuong <quocvietvuong@Quocs-Mac-mini.local>
2025-01-20 10:48:54 +0700

    add images

--- models/campground.js
+++ models/campground.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String,
 })
 
-const Campground = mongoose.model('Campground', CampgroundSchema)
-module.exports = Campground
+module.exports = mongoose.model('Campground', CampgroundSchema)
