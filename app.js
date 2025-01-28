e871c8ed8a8f389001689467c4477913549a2a6e
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-01-28 22:36:04 +0700

    delete reviews

--- app.js
+++ app.js
@@ -108,6 +108,14 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) 
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
+    const { id, reviewId } = req.params
+    console.log("id: ", id, "reviewId: ", reviewId)
+    await Campground.findByIdAndUpdate(id, {$pull: { reviews: reviewId }})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
+}))
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
