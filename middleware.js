aaae73ba4cf3e8f422cef1d3f5de89f4ed1fb200
Vicky Vuong <quocviet.vuong2810@gmail.com>
2025-02-08 00:27:40 +0700

    add campground authorization middleware and refactor

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const { campgroundSchema } = require('./schemas')
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground')
+
 const isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.returnTo = req.originalUrl
@@ -14,4 +18,25 @@ const storeReturnTo = (req, res, next) => {
   next()
 }
 
-module.exports = { isLoggedIn, storeReturnTo }
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body)
+  if (error) {
+      const msg = error.details.map(el => el.message).join(',')
+      throw new ExpressError(msg, 400)
+  } else {
+      next()
+  }
+}
+
+const isAuthor = async(req, res, next) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    console.log('entered isAuthor middleware, author: ', campground.author, ' req.user._id: ', req.user._id)
+    if(!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
+}
+
+module.exports = { isLoggedIn, storeReturnTo, validateCampground, isAuthor }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
-const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas')
-const { isLoggedIn } = require('../middleware')
+const Campground = require('../models/campground')
+const { validateCampground, isLoggedIn, isAuthor } = require('../middleware')
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -14,16 +12,6 @@ router.get('/', catchAsync(async (req, res) => {
 router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new')
 })
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body)
-  if (error) {
-      const msg = error.details.map(el => el.message).join(',')
-      throw new ExpressError(msg, 400)
-  } else {
-      next()
-  }
-}
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req,res, next) => {
     const campground = new Campground(req.body.campground)
@@ -43,7 +31,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground })
 }))
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     if (!campground) {
         req.flash('error', 'Cannot find that campground!')
@@ -53,7 +41,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground })
 }))
 
-router.put('/:id', isLoggedIn, catchAsync(async (req, res, next) => {
+router.put('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
     try {
         const { id } = req.params;
         const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
@@ -64,7 +52,7 @@ router.put('/:id', isLoggedIn, catchAsync(async (req, res, next) => {
     }
 }))
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
     req.flash('success', 'Successfully deleted campground')
